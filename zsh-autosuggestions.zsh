f8a2f3fc8c0b4c3afc3d3713557f6531af15b585
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-09-26 12:31:27 +0700

    lower cutoff period

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -79,7 +79,7 @@ ZSH_AUTOSUGGEST_LAST_MODIFY_TIME=$EPOCHREALTIME
 
 # Lower bound for the time between changes that will cause autosuggest
 # to suspend suggestions for the current edit.
-ZSH_AUTOSUGGEST_CUTOFF_PERIOD=0.05
+ZSH_AUTOSUGGEST_CUTOFF_PERIOD=0.02
 
 #--------------------------------------------------------------------#
 # Handle Deprecated Variables/Widgets                                #
