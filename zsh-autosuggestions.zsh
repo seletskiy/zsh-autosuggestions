cf6976e81b4bd2a8765ac92496214e4cf7113b69
Stanislav Seletskiy <s.seletskiy@gmail.com>
2017-09-02 23:48:45 +0300

    allow overwrite accept widgets

--- zsh-autosuggestions.zsh
+++ zsh-autosuggestions.zsh
@@ -42,37 +42,41 @@ ZSH_AUTOSUGGEST_STRATEGY=default
 
 # Widgets that clear the suggestion
 ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(
-	history-search-forward
-	history-search-backward
-	history-beginning-search-forward
-	history-beginning-search-backward
-	history-substring-search-up
-	history-substring-search-down
-	up-line-or-history
-	down-line-or-history
-	accept-line
+    history-search-forward
+    history-search-backward
+    history-beginning-search-forward
+    history-beginning-search-backward
+    history-substring-search-up
+    history-substring-search-down
+    up-line-or-history
+    down-line-or-history
+    accept-line
 )
 
-# Widgets that accept the entire suggestion
-ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(
-	forward-char
-	end-of-line
-	vi-forward-char
-	vi-end-of-line
-)
+if [ ! "${(t)ZSH_AUTOSUGGEST_ACCEPT_WIDGETS}" ]; then
+    # Widgets that accept the entire suggestion
+    ZSH_AUTOSUGGEST_ACCEPT_WIDGETS=(
+        forward-char
+        end-of-line
+        vi-forward-char
+        vi-end-of-line
+    )
+fi
 
 # Widgets that accept the entire suggestion and execute it
 ZSH_AUTOSUGGEST_EXECUTE_WIDGETS=(
 )
 
-# Widgets that accept the suggestion as far as the cursor moves
-ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(
-	forward-word
-	vi-forward-word
-	vi-forward-word-end
-	vi-forward-blank-word
-	vi-forward-blank-word-end
-)
+if [ ! "${(t)ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS}" ]; then
+    # Widgets that accept the suggestion as far as the cursor moves
+    ZSH_AUTOSUGGEST_PARTIAL_ACCEPT_WIDGETS=(
+        forward-word
+        vi-forward-word
+        vi-forward-word-end
+        vi-forward-blank-word
+        vi-forward-blank-word-end
+    )
+fi
 
 # EPOCH of the last changed widget call
 ZSH_AUTOSUGGEST_LAST_MODIFY_TIME=$EPOCHREALTIME
